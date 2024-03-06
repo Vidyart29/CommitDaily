481f87d2f089773eef0b60753e5c6a9cad80144f
vidya rautela <vidya.rautela.28@gmail.com>
2024-03-07 01:21:04 +0530

    reverse no in java

--- src/com/conditionals_loops/java/ReverseNo.java
+++ src/com/conditionals_loops/java/ReverseNo.java
@@ -1,16 +1,14 @@
 package com.conditionals_loops.java;
 
-import java.util.Scanner;
+
 
 //Reverse A String In Java
-public class ArrayListProgram {
+public class ReverseNo {
 
 	public static void main(String[] args) {
 		
 		System.out.println("reverse a nos");
 		
-//		Scanner inp = new Scanner(System.in);
-//		int num=inp.nextInt();
 		
 		int num=567890;
 		int ans = 0;
