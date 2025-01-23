0e222532c17c461b7c5d726528a82449b6937be8
vidyarautela <vane28ssavdr@gmail.com>
2025-01-23 09:48:57 +0530

    printing arrays as matrix using tostring

--- src/com/arrays/java/TwoDArrays.java
+++ src/com/arrays/java/TwoDArrays.java
@@ -1,5 +1,6 @@
 package com.arrays.java;
 
+import java.util.Arrays;
 //complete code to input ele and print a multidimension array, 3d becomes diff to visualize but could be done.
 import java.util.Scanner;
 
@@ -22,11 +23,19 @@ public class TwoDArrays {
 			}
 		}
 		System.out.println("printing 2d array: ");
+//		for (int i = 0; i < arr.length; i++) {
+//			for (int j = 0; j < arr[i].length; j++) {
+//				System.out.print(arr[i][j]+ " ");
+//			}
+//			System.out.println();
+//		}
+		
 		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr[i].length; j++) {
-				System.out.print(arr[i][j]+ " ");
-			}
-			System.out.println();
+//		this because, every individual  of outside array is an array itself and here it running arrays.tostring for every i'th row element
+		System.out.println(Arrays.toString(arr[i]));
+//		[1, 2, 3]
+//		[4, 5, 6]
+//		[7, 8, 9]
 		}
 	}
 }
